a9537ceba9a867c5d3b366c2c6fcb5b4173d905f
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2021-12-01 02:30:45 +0000

    why the hell does the stupid extra line keep showing up at the beginning instead of at the end of the output (this commit doesn't have it for that reason)

---
+++ typewriter.js
@@ -0,0 +1,8 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 100;
+for (const char of sentence) {
+    setTimeout(() => {
+        process.stdout.write(char);
+    }, delay)
+    delay += 100;
+}
